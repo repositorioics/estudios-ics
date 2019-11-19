7d0c12cbee1843bd00069760b0ba2463651fef88
Miguel Salinas <msalinas@icsnicaragua.org>
2019-11-19 09:09:02 -0600

    Corrección hemodinámica: positivo Servicio consultar datos participante (para huella digital)

---
+++ src/main/java/ni/org/ics/estudios/dto/DatosParticipante.java
@@ -0,0 +1,227 @@
+package ni.org.ics.estudios.dto;
+
+import java.util.Date;
+
+/**
+ * Created by Miguel Salinas on 17/07/2019.
+ * V1.0
+ */
+public class DatosParticipante {
+
+    private Integer codigo;
+    private String nombre1;
+    private String nombre2;
+    private String apellido1;
+    private String apellido2;
+    private String sexo;
+    private Date fechaNac;
+    private String nombre1Padre;
+    private String nombre2Padre;
+    private String apellido1Padre;
+    private String apellido2Padre;
+    private String nombre1Madre;
+    private String nombre2Madre;
+    private String apellido1Madre;
+    private String apellido2Madre;
+    private Integer codigoCasa;
+    private Integer codigoBarrio;
+    private String nombreBarrio;
+    private String direccion;
+    private String manzana;
+    private String estudios;
+    private Integer estPart;
+    private String tutor;
+    private String relacionFamTutor;
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre1() {
+        return nombre1;
+    }
+
+    public void setNombre1(String nombre1) {
+        this.nombre1 = nombre1;
+    }
+
+    public String getNombre2() {
+        return nombre2;
+    }
+
+    public void setNombre2(String nombre2) {
+        this.nombre2 = nombre2;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public Date getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setFechaNac(Date fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+
+    public String getNombre1Padre() {
+        return nombre1Padre;
+    }
+
+    public void setNombre1Padre(String nombre1Padre) {
+        this.nombre1Padre = nombre1Padre;
+    }
+
+    public String getNombre2Padre() {
+        return nombre2Padre;
+    }
+
+    public void setNombre2Padre(String nombre2Padre) {
+        this.nombre2Padre = nombre2Padre;
+    }
+
+    public String getApellido1Padre() {
+        return apellido1Padre;
+    }
+
+    public void setApellido1Padre(String apellido1Padre) {
+        this.apellido1Padre = apellido1Padre;
+    }
+
+    public String getApellido2Padre() {
+        return apellido2Padre;
+    }
+
+    public void setApellido2Padre(String apellido2Padre) {
+        this.apellido2Padre = apellido2Padre;
+    }
+
+    public String getNombre1Madre() {
+        return nombre1Madre;
+    }
+
+    public void setNombre1Madre(String nombre1Madre) {
+        this.nombre1Madre = nombre1Madre;
+    }
+
+    public String getNombre2Madre() {
+        return nombre2Madre;
+    }
+
+    public void setNombre2Madre(String nombre2Madre) {
+        this.nombre2Madre = nombre2Madre;
+    }
+
+    public String getApellido1Madre() {
+        return apellido1Madre;
+    }
+
+    public void setApellido1Madre(String apellido1Madre) {
+        this.apellido1Madre = apellido1Madre;
+    }
+
+    public String getApellido2Madre() {
+        return apellido2Madre;
+    }
+
+    public void setApellido2Madre(String apellido2Madre) {
+        this.apellido2Madre = apellido2Madre;
+    }
+
+    public Integer getCodigoCasa() {
+        return codigoCasa;
+    }
+
+    public void setCodigoCasa(Integer codigoCasa) {
+        this.codigoCasa = codigoCasa;
+    }
+
+    public Integer getCodigoBarrio() {
+        return codigoBarrio;
+    }
+
+    public void setCodigoBarrio(Integer codigoBarrio) {
+        this.codigoBarrio = codigoBarrio;
+    }
+
+    public String getNombreBarrio() {
+        return nombreBarrio;
+    }
+
+    public void setNombreBarrio(String nombreBarrio) {
+        this.nombreBarrio = nombreBarrio;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getManzana() {
+        return manzana;
+    }
+
+    public void setManzana(String manzana) {
+        this.manzana = manzana;
+    }
+
+    public String getEstudios() {
+        return estudios;
+    }
+
+    public void setEstudios(String estudios) {
+        this.estudios = estudios;
+    }
+
+    public Integer getEstPart() {
+        return estPart;
+    }
+
+    public void setEstPart(Integer estPart) {
+        this.estPart = estPart;
+    }
+
+    public String getTutor() {
+        return tutor;
+    }
+
+    public void setTutor(String tutor) {
+        this.tutor = tutor;
+    }
+
+    public String getRelacionFamTutor() {
+        return relacionFamTutor;
+    }
+
+    public void setRelacionFamTutor(String relacionFamTutor) {
+        this.relacionFamTutor = relacionFamTutor;
+    }
+}
