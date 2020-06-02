b6e333ebad6c5a3e60b1ea064b64dcd399cc79cc
LserranoMedrano <lserranoicsnicaragua@gmail.org>
2020-06-02 09:45:28 -0600

    Covid19-Service

--- src/main/java/ni/org/ics/estudios/service/covid/CovidService.java
+++ src/main/java/ni/org/ics/estudios/service/covid/CovidService.java
@@ -20,8 +20,6 @@ public class CovidService {
     private SessionFactory sessionFactory;
 
 
-
-
     public ParticipanteCohorteFamilia getParticipanteCHFByCodigo(Integer codigo){
         Session session = sessionFactory.getCurrentSession();
         Query query = session.createQuery("from ParticipanteCohorteFamilia where participante.codigo = :codigo");
